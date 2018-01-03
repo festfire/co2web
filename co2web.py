22b4a14eb7d3e4b835ae8f6d18a598d14d9c8e83
festfire <festfire@gmail.com>
2018-01-03 08:56:04 +0000

    deleted bottle web server, 1 hour x axis

--- co2web.py
+++ co2web.py
@@ -1,28 +1,15 @@
 import serial
 import os
+import sched
 import time
 import matplotlib
 import matplotlib.pyplot as plt
-import socket
-import threading
-from bottle import route, run
-from bottle import get, post, request, response, redirect
-from bottle import static_file
 import matplotlib.pyplot as plt
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('8.8.8.8', 1))
-ip = s.getsockname()[0]
-s.close()
+path = '/dev/shm'
 rc = '\xFF\x01\x86\x00\x00\x00\x00\x00\x79'
 s = serial.Serial('/dev/ttyS0', baudrate = 9600, timeout = 1)
-data = []
-
-def threaded(fn):
-    def wrapper(*args, **kwargs):
-        threading.Thread(target=fn, args=args, kwargs=kwargs).start()
-    return wrapper
-
+data = list(xrange(360))
+timer = sched.scheduler(time.time, time.sleep)
 def readSensor():
     resp_len = 9
     resp = s.read(resp_len)
@@ -31,22 +18,20 @@ def readSensor():
         crc16 =((sum(payload) % 256) ^ 0xFF) + 1
         return payload
 
-@threaded
-def readLoop():
-    while 1:
-        s.write(rc)
-        payload = readSensor()
-        concentration = payload[1] * 256 + payload[2]
-        data.append(concentration)
-        time.sleep(5)
-
-@route('/co2')
-def co2():
+def readLoop(sc):
+    timer.enter(10, 1, readLoop, (sc,))
+    s.write(rc)
+    payload = readSensor()
+    concentration = payload[1] * 256 + payload[2]
+    data.append(concentration)
+    data.pop(0)
     plt.plot(data)
     plt.ylabel('co2, ppm')
+    plt.xticks([0, 90, 180, 270, 360], ('-60', '-45', '-30', '-15', '0') )
+    plt.xlabel('time, m')
     plt.grid(True)
-    plt.savefig('conc.png', fmt='png')
-    return static_file('conc.png', os.getcwd())
+    plt.savefig(path +'/conc.png', fmt='png')
+    plt.clf()
 
-readLoop()
-run(host=ip, port=80, debug=False)
+timer.enter(10, 1, readLoop, (timer,))
+timer.run()
